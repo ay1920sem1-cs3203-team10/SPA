0fd574b751e2d5fe0d1036a11a0dde89c0da3552
KhorSL <stephenksl@gmail.com>
2019-08-22 16:53:23 +0800

    PKB.h: update VarTable API

--- Code10/source/PKB.h
+++ Code10/source/PKB.h
@@ -7,6 +7,8 @@
 
 using namespace std;
 typedef short PROC;
+typedef int INDEX;
+typedef int INTEGER;
 
 class TNode;
 
@@ -14,8 +16,13 @@ class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
-	static VarTable* varTable; 
+	static VarTable* varTable;
 	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
+	static TNode* getRootAST(PROC p);
 
+	INDEX insertVariable(std::string variableName);
+	std::string getVariableName(INDEX index);
+	INDEX getVariableIndex(std::string variableName);
+	INTEGER getSize();
+	std::unordered_map<std::string, INTEGER> PKB::getVariables()
 };
